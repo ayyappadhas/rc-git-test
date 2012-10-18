193a91ab51d28a2c67b53e38d956afcdd44cc0b4
Ayyappadhas <anishkb003@gmail.com>
2012-10-18 10:24:37 +0530

    Test commit

--- akb-template/application/config/database.php
+++ akb-template/application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '12.0.0.1';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
 $db['default']['database'] = '';
