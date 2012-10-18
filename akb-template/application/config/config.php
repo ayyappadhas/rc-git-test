7844fb81195cee20f7f3b102e0d04f3f2a1cf4cf
Ayyappadhas <anishkb003@gmail.com>
2012-10-18 10:25:58 +0530

    test commit 2

--- akb-template/application/config/config.php
+++ akb-template/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '192.168.0.104';
+$config['base_url']	= 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
